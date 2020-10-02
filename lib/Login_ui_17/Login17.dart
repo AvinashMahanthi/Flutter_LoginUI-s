36c1ecfe93b4133ab07b12efb5da3fa778a69eff
Avinash <avinashfab5678@gmail.com>
2020-10-02 20:56:22 +0530

    day-17 initial commit

---
+++ lib/Login_ui_17/Login17.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Login17 extends StatefulWidget {
+  @override
+  _Login17State createState() => _Login17State();
+}
+
+class _Login17State extends State<Login17> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
