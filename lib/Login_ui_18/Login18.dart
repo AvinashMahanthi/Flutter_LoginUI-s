186db16650253fbdeb1fec407a5594b26008bcce
Avinash <avinashfab5678@gmail.com>
2020-10-03 17:44:49 +0530

    day-18 initial commit

---
+++ lib/Login_ui_18/Login18.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Login18 extends StatefulWidget {
+  @override
+  _Login18State createState() => _Login18State();
+}
+
+class _Login18State extends State<Login18> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
