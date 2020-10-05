db4f8706f4087669565ef8ad117deb17ab8f641e
Avinash <avinashfab5678@gmail.com>
2020-10-05 22:45:22 +0530

    Day19 initial

---
+++ lib/Login_ui_19/WelcomePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen19 extends StatefulWidget {
+  @override
+  _WelcomeScreen19State createState() => _WelcomeScreen19State();
+}
+
+class _WelcomeScreen19State extends State<WelcomeScreen19> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0041C4),
+    );
+  }
+}
